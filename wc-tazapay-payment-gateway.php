2032985f35e3a2bc65954c5811bbf38218723b89
Pritesh.Modi <pritesh.lr@gmail.com>
2021-09-03 07:53:52 +0000

    Tazapay table created

--- wc-tazapay-payment-gateway.php
+++ wc-tazapay-payment-gateway.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name:       WooCommerce TazaPay Payment Gateway
  * Plugin URI:        https://www.logicrays.com/
- * Description:       Pay with your TazaPay via our super-cool payment gateway.
+ * Description:       Pay with your TazaPay via our super-cool payment gateway..
  * Version:           1.0.0
  * Author:            Logicrays
  * Author URI:        https://www.logicrays.com
@@ -14,22 +14,59 @@
 
 define( 'TAZAPAY_CSS_JSS_VERISON', time() );
 define( 'TAZAPAY_PUBLIC_ASSETS_DIR', plugins_url('assets/', __FILE__));
- /*
- * This action hook registers our PHP class as a WooCommerce payment gateway
- */
-add_filter( 'woocommerce_payment_gateways', 'tazapay_add_gateway_class' );
+$plugin = plugin_basename(__FILE__);
 
+register_activation_hook( __FILE__, 'tazapay_user_install' );
+
+function tazapay_user_install() {
+    global $wpdb;
+
+    $table_name      = $wpdb->prefix . 'tazapay_user';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        account_id varchar(255) NOT NULL,
+        user_type varchar(255) NOT NULL,
+        email varchar(255) NOT NULL,
+        first_name varchar(255) NOT NULL,
+        last_name varchar(255) NOT NULL,
+        contact_code varchar(255) NOT NULL,
+        contact_number varchar(255) NOT NULL,
+        country varchar(255) NOT NULL,
+        ind_bus_type varchar(255) NOT NULL,
+        business_name varchar(255) NOT NULL,
+        partners_customer_id varchar(255) NOT NULL,
+        environment varchar(255) NOT NULL,
+        created varchar(255) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+/*
+* This action hook registers our PHP class as a WooCommerce payment gateway
+*/
+add_filter( 'woocommerce_payment_gateways', 'tazapay_add_gateway_class' );
 function tazapay_add_gateway_class( $gateways ) {
     $gateways[] = 'WC_TazaPay_Gateway'; // your class name is here
     return $gateways;
 }
 
+/*
+* Frontend css
+*/
 add_action( 'wp_enqueue_scripts', 'tazapay_frontend_enqueue_styles' );
 function tazapay_frontend_enqueue_styles()
 {
     wp_enqueue_style('tazapay_form_css', TAZAPAY_PUBLIC_ASSETS_DIR . 'css/tazapay-frontend.css', array(), TAZAPAY_CSS_JSS_VERISON, 'all');    
 }
 
+/*
+* Backend css and js
+*/
 add_action( 'admin_enqueue_scripts', 'tazapay_enqueue_styles' );
 function tazapay_enqueue_styles()
 {
@@ -38,8 +75,28 @@ function tazapay_enqueue_styles()
     wp_enqueue_script( 'tazapay_public_js', TAZAPAY_PUBLIC_ASSETS_DIR . 'js/tazapay-form.js', array('jquery'), TAZAPAY_CSS_JSS_VERISON, true);
 }
 
+/*
+* Plugin settings page
+*/
+add_filter("plugin_action_links_$plugin", 'tazapay_plugin_settings_link' );
+function tazapay_plugin_settings_link($links) { 
+  $settings_link = '<a href="admin.php?page=wc-settings&tab=checkout&section=tz_tazapay">Settings</a>'; 
+  array_unshift($links, $settings_link); 
+  return $links; 
+}
 
+/*
+ * The class itself, please note that it is inside plugins_loaded action hook
+ */
+add_action( 'plugins_loaded', 'tazapay_init_gateway_class' );
+function tazapay_init_gateway_class() {
+    require 'includes/class-wc-tazapay-payment-gateway.php';
+    require 'includes/class-wc-tazapay-user-list-table.php';
+    require 'includes/class-wc-tazapay-account-form.php';
+    require 'includes/wc-order-status-change.php';
 
+    load_plugin_textdomain( 'wc-tp-payment-gateway', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
 
 // register_activation_hook(__FILE__, 'add_tazapay_signup_page');
 // function add_tazapay_signup_page() {
@@ -55,15 +112,3 @@ function tazapay_enqueue_styles()
 //     wp_insert_post( $tazapay_signup_page );
 // }
 
-/*
- * The class itself, please note that it is inside plugins_loaded action hook
- */
-add_action( 'plugins_loaded', 'tazapay_init_gateway_class' );
-function tazapay_init_gateway_class() {
-    require 'includes/class-wc-tazapay-payment-gateway.php';
-    require 'includes/class-wc-tazapay-user-list-table.php';
-    require 'includes/class-wc-tazapay-account-form.php';
-    require 'includes/wc-order-status-change.php';
-
-    load_plugin_textdomain( 'wc-tp-payment-gateway', false, basename( dirname( __FILE__ ) ) . '/languages/' );
-}
